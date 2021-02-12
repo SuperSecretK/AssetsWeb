2014fffd07d0d3dfd4b22364810f91086f5d818c
BrianDoan <brianddoan@gmail.com>
2021-02-13 00:18:44 +0700

    Finish custom Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,27 +1,50 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Col } from "react-bootstrap";
 
 export default class InputForm extends Component {
   /**
    * options: {
-   *  path: '/path',
-   *  time: true/false,
-   *  inputs: [...{name, type}],
-   *  button: name
+    *  path: 'api/profile',
+    *  query: String,
+    *  dropdownQuery: String,
+    *  time: Boolean,
+    *  label: Boolean,
+    *  placeholder: Boolean,
+    *  inputs: [...{name, type}],
+    *  button: String
    * }
    */
   constructor(props) {
     super(props);
-    this.state = { };
+    this.state = {
+      query: this.props.options.dropdownQuery
+    };
+  }
+
+  renderSelections(input) {
+    return input.selections.map((select, index) => (
+      <option key={index}>{select}</option>
+    ));
   }
 
   renderInputs() {
-    return this.props.options.inputs.map((input, index) => (
+    const opt = this.props.options;
+    return opt.inputs.map((input, index) => (
+      <Col>
       <Form.Group key={index}>
-        <Form.Label>{input.name}</Form.Label>
-        <Form.Control type={input.type} name={input.name} onChange={this.onChange} value={this.state[`${input.name}`]}/>
+        {opt.label &&
+          <Form.Label>{input.name}</Form.Label>
+        }
+        {input.type !== 'dropdown' ? (
+          <Form.Control type={input.type} name={input.name} onChange={this.onChange} value={this.state[`${input.name}`]} placeholder={opt.placeholder && `${input.name}`}/>
+        ) : (
+          <Form.Control as="select" name={input.name} onChange={this.onChange} defaultValue={input.name}>
+            {this.renderSelections(input)}
+          </Form.Control>
+        )}
       </Form.Group>
+      </Col>
     ));
   }
 
@@ -29,10 +52,23 @@ export default class InputForm extends Component {
     let obj = {};
     obj[e.target.name] = e.target.value; 
     this.setState(obj);
+    if (e.target.type !== 'text' &&
+        e.target.type !== 'number' &&
+        e.target.type !== 'password') {
+          this.setState({query: e.target.value});
+    }
   }
 
   postReq() {
-    axios.put(this.props.options.path, this.state)
+    let path;
+    const opt = this.props.options;
+    if (opt.dropdownQuery !== '') {
+      path = `${opt.path}/${this.state.query}`;
+    } else {
+      path = `${opt.path}/${opt.query}`;
+    }
+    console.log(path);
+    axios.put(path, this.state)
     .then(res => console.log(res.data))
     .catch(err => console.log(err));
   }
@@ -53,10 +89,14 @@ export default class InputForm extends Component {
   render() {
     return (
       <Form onSubmit={this.onSubmit}>
+        <Form.Row>
         {this.renderInputs()}
+        <Col>
         <Button className="login-btn" block style={{backgroundColor: "#f36523"}} type="submit" id="button">
           {this.props.options.button}
         </Button>
+        </Col>
+        </Form.Row>
       </Form>
     );
   }
